bb41457775a33b852debab47dc39e97e53a36e72
Akil0814 <140119093+Akil0814@users.noreply.github.com>
2025-08-25 22:27:00 -0700

    Update aviation_battleship.h

--- aviation_battleship.h
+++ aviation_battleship.h
@@ -5,7 +5,7 @@ class AviationBattleship :public Ship
 public:
 	AviationBattleship()
 	{
-		Ship::init_ship(ResourcesManager::instance()->get_texture(ResID::Tex_Ship_AviationBattleship), 5, 5, 4, 0, SkillType::Attack_3x3,2);
+		Ship::init_ship(ResourcesManager::instance()->get_texture(ResID::Tex_Ship_AviationBattleship), 5, 5, 3, 0, SkillType::Attack_3x3,2);
 	}
 	~AviationBattleship()
 	{	}
