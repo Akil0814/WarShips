93a965c6bed555e2616df6be3bd3093c08320f3c
Akil0814 <140119093+Akil0814@users.noreply.github.com>
2025-08-25 22:27:35 -0700

    Update aircraft_carrier.h

--- aircraft_carrier.h
+++ aircraft_carrier.h
@@ -5,7 +5,7 @@ class AircraftCarrier :public Ship
 public:
 	AircraftCarrier()
 	{
-		Ship::init_ship(ResourcesManager::instance()->get_texture(ResID::Tex_Ship_AircraftCarrier), 5, 5, 3, 0, SkillType::Detect_13C,2);
+		Ship::init_ship(ResourcesManager::instance()->get_texture(ResID::Tex_Ship_AircraftCarrier), 5, 5, 4, 0, SkillType::Detect_13C,2);
 	}
 	~AircraftCarrier(){}
 
